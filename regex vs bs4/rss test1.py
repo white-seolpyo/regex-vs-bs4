0c4120ca2f648aff5773586b4bfdb92c681eafaf
하얀설표 <white-seolpyo@naver.com>
2024-12-13 10:13:57 +0900

    Add files via upload

---
+++ regex vs bs4/rss test1.py
@@ -0,0 +1,130 @@
+from pprint import pprint
+from re import DOTALL, findall, search, sub
+from time import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse(text):
+    t = time()
+
+    soup = BeautifulSoup(text, 'xml')
+    list_item = soup.select('item')
+    result = []
+    for item in list_item:
+        title = item.find('title').text
+        author = item.find('author').text
+        link = item.find('link').text
+        desc = item.find('description').text
+        date = item.find('pubDate').text
+        tag = item.find('tag').text
+        i = {
+            'title': title,
+            'author': author,
+            'link': link,
+            'desc': desc,
+            'date': date,
+            'tag': tag
+        }
+        result.append(i)
+
+    tt = time() - t
+    text = f'parse, {tt=}'
+    return (result, text)
+
+def regex(text):
+    t = time()
+
+    list_item = findall('<item>(.+?)</item>', text, DOTALL)
+    result = []
+    for item in list_item:
+        title = ' '.join([i for i in search('<title><!\[CDATA\[(.+?)\]\]></title>', item, DOTALL).group(1).split() if i])
+        author = search('<author>(.+?)</author>', item, DOTALL).group(1)
+        link = search('<link><!\[CDATA\[(.+?)\]\]></link>', item, DOTALL).group(1)
+        desc = search('<description><!\[CDATA\[(.+?)\]\]></description>', item, DOTALL).group(1)
+        date = search('<pubDate>(.+?)</pubDate>', item, DOTALL).group(1)
+        tag = search('<tag><!\[CDATA\[(.*?)\]\]></tag>', item, DOTALL).group(1)
+        i = {
+            'title': title,
+            'author': author,
+            'link': link,
+            'desc': desc,
+            'date': date,
+            'tag': tag
+        }
+        result.append(i)
+
+    tt = time() - t
+    text = f'regex, {tt=}'
+    return (result, text)
+
+
+url = 'https://rss.blog.naver.com/blogpeople.xml'
+
+r = requests.get(url)
+t = r.text
+# print(t)
+
+a = parse(t)
+b = regex(t)
+
+pprint(a[0][1], sort_dicts=False)
+pprint(b[0][1], sort_dicts=False)
+print()
+pprint(a[0][2], sort_dicts=False)
+pprint(b[0][2], sort_dicts=False)
+print()
+print(f'{a[1]=}')
+print(f'{b[1]=}')
+
+"""
+{'title': '[2024 블로그 리포트 OPEN] 풍성한 데이터가 가득한 블로그 마을로 초대합니다  ️',
+ 'author': 'blogpeople',
+ 'link': 'https://blog.naver.com/blogpeople/223690028011?fromRss=true&trackingCode=rss',
+ 'desc': '안녕하세요, 네이버 블로그팀입니다! 2024년에도 블로그와 함께해 주신 여러분들을 위해 올해도 어김없이 찾아온 블로그 연말 '
+         '결산입니다. 매해 블로그를 찾아주시고, 아껴주시고, 애정해 주시는 여러분들의 사랑에 보답하기 위해 이번 블로그 연말 결산은 '
+         '더욱 알차게 구성하였는데요! 블로그 곳곳에 숨겨진 의미 있는 기록들을 살펴보는 네이버 블로그 리포트 그리고 나의 블로그홈 '
+         '데이터를 기반으로 그려진 내 블로그 마을을 확인해 보는 마이 블로그 리포트를 공개합니다. (개봉 박두) 올해도 기록 맛집 '
+         '/ 블로그가 핫플일 수밖에 없는 이유 2024 네이버 블로그 리포트 벌써 막바지를 향해 달려가고 있는 2024년, 블로그와 '
+         '함께한 올해는 여.......',
+ 'date': 'Wed, 11 Dec 2024 17:42:44 +0900',
+ 'tag': '2024마이블로그리포트,2024네이버블로그리포트,2024블로그연말결산,올해도많관부'}
+{'title': '[2024 블로그 리포트 OPEN] 풍성한 데이터가 가득한 블로그 마을로 초대합니다 ️',
+ 'author': 'blogpeople',
+ 'link': 'https://blog.naver.com/blogpeople/223690028011?fromRss=true&trackingCode=rss',
+ 'desc': '안녕하세요, 네이버 블로그팀입니다! 2024년에도 블로그와 함께해 주신 여러분들을 위해 올해도 어김없이 찾아온 블로그 연말 '
+         '결산입니다. 매해 블로그를 찾아주시고, 아껴주시고, 애정해 주시는 여러분들의 사랑에 보답하기 위해 이번 블로그 연말 결산은 '
+         '더욱 알차게 구성하였는데요! 블로그 곳곳에 숨겨진 의미 있는 기록들을 살펴보는 네이버 블로그 리포트 그리고 나의 블로그홈 '
+         '데이터를 기반으로 그려진 내 블로그 마을을 확인해 보는 마이 블로그 리포트를 공개합니다. (개봉 박두) 올해도 기록 맛집 '
+         '/ 블로그가 핫플일 수밖에 없는 이유 2024 네이버 블로그 리포트 벌써 막바지를 향해 달려가고 있는 2024년, 블로그와 '
+         '함께한 올해는 여.......',
+ 'date': 'Wed, 11 Dec 2024 17:42:44 +0900',
+ 'tag': '2024마이블로그리포트,2024네이버블로그리포트,2024블로그연말결산,올해도많관부'}
+
+{'title': '[공지] 포스트 종료에 따른 블로그 이전 안내',
+ 'author': 'blogpeople',
+ 'link': 'https://blog.naver.com/blogpeople/223687587849?fromRss=true&trackingCode=rss',
+ 'desc': '안녕하세요. 네이버 블로그팀입니다. 2025.04.30 (수), 네이버 포스트가 종료됩니다. 포스트 공지 보러 가기 '
+         '&gt; 포스트는 아쉽게 종료되지만 포스트에서의 창작과 구독 활동은 블로그에서 계속 이어가실 수 있답니다! '
+         '&quot;포스트에 썼던 내 글, 블로그에 그대로 옮겨지나요?&quot; &quot;포스트에서 즐겨 보던 글, 블로그에서 '
+         '어떻게 보나요?&quot; 위와 같은 궁금증이 있으시다면! 블로그에서, 포스트 활동을 이어가는 방법에 대해 아래 내용을 '
+         '자세히 확인해 주세요. 이전된 게시글은 어떻게 보이나요? 블로그로 게시글 이전을 신청하신 경우, 포스트에서 전체 공개로 '
+         '발행된 게시글이 블로그의 최신 에디터 포맷으로 변환됩니다. ※기본형.......',
+ 'date': 'Mon, 09 Dec 2024 16:34:58 +0900',
+ 'tag': ' '}
+{'title': '[공지] 포스트 종료에 따른 블로그 이전 안내',
+ 'author': 'blogpeople',
+ 'link': 'https://blog.naver.com/blogpeople/223687587849?fromRss=true&trackingCode=rss',
+ 'desc': '안녕하세요. 네이버 블로그팀입니다. 2025.04.30 (수), 네이버 포스트가 종료됩니다. 포스트 공지 보러 가기 '
+         '&gt; 포스트는 아쉽게 종료되지만 포스트에서의 창작과 구독 활동은 블로그에서 계속 이어가실 수 있답니다! '
+         '&quot;포스트에 썼던 내 글, 블로그에 그대로 옮겨지나요?&quot; &quot;포스트에서 즐겨 보던 글, 블로그에서 '
+         '어떻게 보나요?&quot; 위와 같은 궁금증이 있으시다면! 블로그에서, 포스트 활동을 이어가는 방법에 대해 아래 내용을 '
+         '자세히 확인해 주세요. 이전된 게시글은 어떻게 보이나요? 블로그로 게시글 이전을 신청하신 경우, 포스트에서 전체 공개로 '
+         '발행된 게시글이 블로그의 최신 에디터 포맷으로 변환됩니다. ※기본형.......',
+ 'date': 'Mon, 09 Dec 2024 16:34:58 +0900',
+ 'tag': ''}
+
+a[1]='parse, tt=0.022998809814453125'
+b[1]='regex, tt=0.002001047134399414'
+"""
---
+++ regex vs bs4/rss test2.py
@@ -0,0 +1,123 @@
+from html import unescape
+from pprint import pprint
+from re import DOTALL, findall, search, sub
+from time import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse(text):
+    t = time()
+
+    soup = BeautifulSoup(text, 'html.parser')
+    list_item = soup.select('ul.list_news > li')
+    result = []
+    for item in list_item:
+        a = item.select_one('a.news_tit')
+        title = a.text
+        author = item.select_one('a.info.press').text
+        link = a['href']
+        desc = item.select_one('a.dsc_txt_wrap').text
+        date = item.select_one('span.info:not(:has(i))').text
+        i = {
+            'title': title,
+            'author': author,
+            'link': link,
+            'desc': desc,
+            'date': date,
+        }
+        result.append(i)
+
+    tt = time() - t
+    text = f'parse, {tt=}'
+    return (result, text)
+
+def regex(text):
+    t = time()
+
+    list_item = findall('<li class="bx".+?>(.+?)</li>', text, DOTALL)
+    result = []
+    for item in list_item:
+        title = search('<a.+class="news_tit.+?>(.+?)</a>', item, DOTALL).group(1)
+        title = sub('<.+?>', '', title)
+        title = unescape(title)
+        author = search('</span>(.+?)</a>', item, DOTALL).group(1)
+        author = sub('<.+?>', '', author)
+        link = findall('<a href="([^"]+?)" class="news_tit', item, DOTALL)
+        # a = findall('<a.+?>', item)
+        # for i in a:
+        #     link = search('href="(.+?)" class="news_tit', i, DOTALL)
+        #     if link:
+        #         link = link.group(1)
+        #         break
+        desc = search('<a.+class="api_txt_lines.+?>(.+?)</a>', item, DOTALL).group(1)
+        desc = sub('<.+?>', '', desc)
+        desc = unescape(desc)
+        date = search('<span class="info">([^<]+?)</span>', item, DOTALL).group(1)
+        i = {
+            'title': title,
+            'author': author,
+            'link': link,
+            'desc': desc,
+            'date': date,
+        }
+        result.append(i)
+
+    tt = time() - t
+    text = f'regex, {tt=}'
+    return (result, text)
+
+
+url = 'https://search.naver.com/search.naver?where=news&query=%EB%84%A4%EC%9D%B4%EB%B2%84&sm=tab_opt&sort=1&photo=3&field=0&pd=3&ds=2020.02.20&de=2022.02.22&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Afrom20200220to20220222&is_sug_officeid=0&office_category=0&service_area=0'
+
+r = requests.get(url)
+t = r.text
+# print(t)
+
+a = parse(t)
+b = regex(t)
+
+pprint(a[0][1], sort_dicts=False)
+pprint(b[0][1], sort_dicts=False)
+print()
+pprint(a[0][2], sort_dicts=False)
+pprint(b[0][2], sort_dicts=False)
+print()
+print(f'{a[1]=}')
+print(f'{b[1]=}')
+
+"""
+{'title': '우크라이나 긴장 고조에 깜짝 놀란 코스피…외국인 3200억 매도 ',
+ 'author': '한겨레언론사 선정',
+ 'link': 'https://www.hani.co.kr/arti/economy/economy_general/1032169.html',
+ 'desc': '시가총액 상위 10개 종목 중 외국인 보유 비중이 50%가 넘는 삼성전자·에스케이(SK)하이닉스·네이버 모두 1%대 '
+         '하락률을 보였다. 외국인은 이날 유가증권시장에서만 3200억원 남짓 주식을 팔아치웠다. 코스닥지수는 전날보다 '
+         '1.83%(16.14) 내린 868.11에 마감했다. 닛케이지수(일본) 등 아시아 주요 지수들도 1%대... ',
+ 'date': '2022.02.22.'}
+{'title': '우크라이나 긴장 고조에 깜짝 놀란 코스피…외국인 3200억 매도 ',
+ 'author': '한겨레언론사 선정',
+ 'link': ['https://www.hani.co.kr/arti/economy/economy_general/1032169.html'],
+ 'desc': '시가총액 상위 10개 종목 중 외국인 보유 비중이 50%가 넘는 삼성전자·에스케이(SK)하이닉스·네이버 모두 1%대 '
+         '하락률을 보였다. 외국인은 이날 유가증권시장에서만 3200억원 남짓 주식을 팔아치웠다. 코스닥지수는 전날보다 '
+         '1.83%(16.14) 내린 868.11에 마감했다. 닛케이지수(일본) 등 아시아 주요 지수들도 1%대... ',
+ 'date': '2022.02.22.'}
+
+{'title': '크래프톤도 NFT 사업 진출… "새 성장동력 확보" ',
+ 'author': '디지털타임스',
+ 'link': 'http://www.dt.co.kr/contents.html?article_no=2022022302101231820001&ref=naver',
+ 'desc': '크래프톤은 이미 네이버제트와 이용자 창작 콘텐츠(User Generated Contents·UGC) 오픈 메타버스 '
+         '프로젝트를 추진 중이다. 다수의 온라인 게임을 개발하며... 네이버와 카카오는 연내 NFT 사업을 본격화한다. 네이버의 '
+         "경우 최근 자회사 스노우와 AI(인공지능) 기업 알체라가 함께 만든 조인트벤처 '팔라'를... ",
+ 'date': '2022.02.22.'}
+{'title': '크래프톤도 NFT 사업 진출… "새 성장동력 확보" ',
+ 'author': '디지털타임스',
+ 'link': ['http://www.dt.co.kr/contents.html?article_no=2022022302101231820001&ref=naver'],
+ 'desc': '크래프톤은 이미 네이버제트와 이용자 창작 콘텐츠(User Generated Contents·UGC) 오픈 메타버스 '
+         '프로젝트를 추진 중이다. 다수의 온라인 게임을 개발하며... 네이버와 카카오는 연내 NFT 사업을 본격화한다. 네이버의 '
+         "경우 최근 자회사 스노우와 AI(인공지능) 기업 알체라가 함께 만든 조인트벤처 '팔라'를... ",
+ 'date': '2022.02.22.'}
+
+a[1]='parse, tt=0.051999568939208984'
+b[1]='regex, tt=0.0010013580322265625'
+"""
